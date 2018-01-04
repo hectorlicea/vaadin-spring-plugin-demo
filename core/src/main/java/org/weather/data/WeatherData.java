e42c85f9ac6fb586bbb3ced0923f71f3d2a076ec
hlicea <hector.licea@datys.cu>
2018-01-04 09:18:56 -0500

    update data

--- core/src/main/java/org/weather/data/WeatherData.java
+++ core/src/main/java/org/weather/data/WeatherData.java
@@ -9,23 +9,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class WeatherData {
-    private String type;
     private String location;
     private Double latitude;
     private Double longitude;
+    private String type;
     private Double value;
     private Double min;
     private Double max;
 
-    public WeatherData(String type, String location, Double latitude, Double longitude, Double value){
-        this(type, location, latitude, longitude, value, null, null);
+    public WeatherData(String location, Double latitude, Double longitude, String type, Double value){
+        this(location, latitude, longitude, type, value, null, null);
     }
 
-    public WeatherData(String type, String location, Double latitude, Double longitude, Double value, Double min, Double max){
-        this.type = type;
+    public WeatherData(String location, Double latitude, Double longitude, String type, Double value, Double min, Double max){
         this.location = location;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.type = type;
         this.value = value;
         this.min = min;
         this.max = max;
--- main/src/main/java/org/weather/service/WeatherServiceImpl.java
+++ main/src/main/java/org/weather/service/WeatherServiceImpl.java
@@ -15,11 +15,11 @@ public class WeatherServiceImpl implements WeatherService {
     @Override
     public List<WeatherData> loadWeatherData() {
         //Generate dummy weather data
-        return Arrays.asList(new WeatherData("rain", "East", 20.173, -74.377, 95.0),
-                new WeatherData("wind", "East",20.431,-74.734, 20.0),
-                new WeatherData("temperature", "East", 20.168, -74.212, 31.0, 25.0, 36.0),
-                new WeatherData("pressure", "East", 20.168, -74.212, 2010.0),
-                new WeatherData("rain", "West", 20.755, -77.629,62.0),
-                new WeatherData("temperature", "North",21.206,-76.365, -5.0, -7.0, 0.0));
+        return Arrays.asList(new WeatherData("East", 20.173, -74.377, "rain", 95.0),
+                new WeatherData("East",20.431,-74.734, "wind", 20.0),
+                new WeatherData("East", 20.168, -74.212, "temperature", 31.0, 25.0, 36.0),
+                new WeatherData("East", 20.168, -74.212, "pressure", 2010.0),
+                new WeatherData("West", 20.755, -77.629,"rain", 62.0),
+                new WeatherData("North",21.206,-76.365, "temperature", -5.0, -7.0, 0.0));
     }
 }
